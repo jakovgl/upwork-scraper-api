9be98f482e250f090410ec7b3a897e7e3f60ec2c
Jakov <jakov@init.hr>
2022-03-09 20:54:02 +0100

    feat: docker-compose file and initial.sql

---
+++ upwork-scraper-server/assets/initial.sql
@@ -0,0 +1,27 @@
+begin;
+
+create table if not exists settings (
+    active boolean,
+    cookie varchar,
+    telegram_api_key varchar,
+    telegram_chat_id varchar,
+    engagement varchar
+);
+
+create table if not exists category (
+    id serial primary key,
+    varchar name
+);
+
+create table if not exists job (
+   title varchar,
+   description varchar,
+   amount int,
+   currency varchar,
+   engagement varchar,
+   proposals_tier varchar,
+   attributes varchar
+);
+
+commit;
+end;
